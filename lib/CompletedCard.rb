db34ce92a836780195db52b07e25160e5f50c784
iainjmitchell <iainjmitchell@gmail.com>
2014-04-28 17:07:39 +0100

    refactoring

---
+++ lib/CompletedCard.rb
@@ -0,0 +1,11 @@
+module AgileTrello
+	class CompletedCard
+		SECONDS_IN_24HRS = (24 * 60 * 60)
+
+		attr_reader :cycle_time
+
+		def initialize(start_date, end_date)
+			@cycle_time = ((end_date - start_date) / SECONDS_IN_24HRS).round(2)
+		end
+	end
+end
--- tests/CycleTimeTests.rb
+++ tests/CycleTimeTests.rb
@@ -3,6 +3,7 @@ require 'rspec-expectations'
 require 'SecureRandom'
 require_relative '../lib/TrelloFactory'
 require_relative '../lib/TrelloCredentials'
+require_relative '../lib/CompletedCard'
 
 class CycleTimeTests < Test::Unit::TestCase	
 	include AgileTrello
@@ -206,16 +207,6 @@ module AgileTrello
 			end
 
 			CompletedCard.new(start_date, end_date)
-		end
-	end
-
-	class CompletedCard
-		SECONDS_IN_24HRS = (24 * 60 * 60)
-
-		attr_reader :cycle_time
-
-		def initialize(start_date, end_date)
-			@cycle_time = ((end_date - start_date) / SECONDS_IN_24HRS).round(2)
 		end
 	end
 
