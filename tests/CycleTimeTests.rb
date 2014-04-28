1cc62e31f499d03075a405ad08f6da9daf54a383
iainjmitchell <iainjmitchell@gmail.com>
2014-04-28 12:12:33 +0100

    adding access token to credentials

--- tests/CycleTimeTests.rb
+++ tests/CycleTimeTests.rb
@@ -11,12 +11,12 @@ class CycleTimeTests < Test::Unit::TestCase
 		expect(@trello_credentials.public_key).to eql(public_key)
 	end 
 
-	# def test_user_connects_to_trello_with_access_token
-	# 	access_token = SecureRandom.uuid
-	# 	mockTrelloFactory = self
-	# 	TrelloCycleTime.new(access_token, mockTrelloFactory) 
-	# 	expect(@trello_credentials.access_token).to eql(access_token)
-	# end 
+	def test_user_connects_to_trello_with_access_token
+		access_token = SecureRandom.uuid
+		mockTrelloFactory = self
+		TrelloCycleTime.new(mockTrelloFactory, access_token: access_token) 
+		expect(@trello_credentials.access_token).to eql(access_token)
+	end 
 
 	def create(trello_credentials)
 		@trello_credentials = trello_credentials
@@ -25,15 +25,16 @@ end
 
 class TrelloCycleTime
 	def initialize(trello_factory = TrelloFactory.new, parameters) 
-		trello_credentials = TrelloCredentials.new(parameters[:public_key])
+		trello_credentials = TrelloCredentials.new(parameters[:public_key], parameters[:access_token])
 		trello_factory.create(trello_credentials) 
 	end
 end
 
 class TrelloCredentials 
-	attr_reader :public_key
+	attr_reader :public_key, :access_token
 
-	def initialize(public_key)
+	def initialize(public_key, access_token)
 		@public_key = public_key
+		@access_token = access_token
 	end
 end
