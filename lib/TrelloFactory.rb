a4b19f5c22efac4f28dbbab66d6be513a43da7ad
iainjmitchell <iainjmitchell@gmail.com>
2014-04-28 12:14:52 +0100

    refactoring

--- lib/TrelloFactory.rb
+++ lib/TrelloFactory.rb
@@ -1,5 +1,12 @@
+require 'trello'
+
 class TrelloFactory 
+	include Trello
+	include Trello::Authorization
+	
 	def create(trello_credentials)
-		
+		Trello::Authorization.const_set :AuthPolicy, OAuthPolicy
+		OAuthPolicy.consumer_credential = OAuthCredential.new trello_credentials.public_key, 'SECRET'
+		OAuthPolicy.token = OAuthCredential.new trello_credentials.access_token, nil
 	end
 end
