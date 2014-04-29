ca83cc0a6de872369429972d620946c855b91893
iainjmitchell <iainjmitchell@gmail.com>
2014-04-29 09:51:28 +0100

    average cycle time calculated

--- tests/CycleTimeTests.rb
+++ tests/CycleTimeTests.rb
@@ -54,8 +54,8 @@ class CycleTimeTests < Test::Unit::TestCase
 		fake_card.add_movement(list_name: start_list_name, date: two_days_ago)
 		fake_card.add_movement(list_name: end_list_name, date: today)
 		board_with_start_and_end_lists = FakeBoard.new
-		board_with_start_and_end_lists.add(FakeList.new('start list'))
-		end_list = FakeList.new('end list')
+		board_with_start_and_end_lists.add(FakeList.new(start_list_name))
+		end_list = FakeList.new(end_list_name)
 		end_list.add(fake_card)
 		board_with_start_and_end_lists.add(end_list)
 		@created_trello = FakeTrello.new(board_id: board_id, board: board_with_start_and_end_lists)
@@ -74,14 +74,39 @@ class CycleTimeTests < Test::Unit::TestCase
 		fake_card.add_movement(list_name: start_list_name, date: two_and_a_bit_days_ago)
 		fake_card.add_movement(list_name: end_list_name, date: today)
 		board_with_start_and_end_lists = FakeBoard.new
-		board_with_start_and_end_lists.add(FakeList.new('start list'))
-		end_list = FakeList.new('end list')
+		board_with_start_and_end_lists.add(FakeList.new(start_list_name))
+		end_list = FakeList.new(end_list_name)
 		end_list.add(fake_card)
 		board_with_start_and_end_lists.add(end_list)
 		@created_trello = FakeTrello.new(board_id: board_id, board: board_with_start_and_end_lists)
 		mockTrelloFactory = self
 		trello_cycle_time = TrelloCycleTime.new(mockTrelloFactory)
 		trello_cycle_time.get(board_id: board_id, start_list: start_list_name, end_list: end_list_name).should eql(2.54)
+	end
+
+	def test_average_cycle_time_of_cards_returned_when_board_has_both_start_and_end_lists_and_multiple_cards_is_in_end_list
+		board_id = SecureRandom.uuid
+		start_list_name = 'start list'
+		end_list_name = 'end list'
+		today = Time.now
+		two_day_cycle_time_card = FakeCard.new 
+		two_days_ago = today - (ONE_DAY * 2)
+		two_day_cycle_time_card.add_movement(list_name: start_list_name, date: two_days_ago)
+		two_day_cycle_time_card.add_movement(list_name: end_list_name, date: today)
+		four_day_cycle_time_card = FakeCard.new 
+		four_days_ago = today - (ONE_DAY * 4)
+		four_day_cycle_time_card.add_movement(list_name: start_list_name, date: four_days_ago)
+		four_day_cycle_time_card.add_movement(list_name: end_list_name, date: today)
+		board_with_start_and_end_lists = FakeBoard.new
+		board_with_start_and_end_lists.add(FakeList.new(start_list_name))
+		end_list = FakeList.new(end_list_name)
+		end_list.add(two_day_cycle_time_card)
+		end_list.add(four_day_cycle_time_card)
+		board_with_start_and_end_lists.add(end_list)
+		@created_trello = FakeTrello.new(board_id: board_id, board: board_with_start_and_end_lists)
+		mockTrelloFactory = self
+		trello_cycle_time = TrelloCycleTime.new(mockTrelloFactory)
+		trello_cycle_time.get(board_id: board_id, start_list: start_list_name, end_list: end_list_name).should eql(3.0)
 	end
 
 	def create(trello_credentials)
@@ -163,13 +188,31 @@ module AgileTrello
 		def initialize(trello_factory = TrelloFactory.new, parameters = {}) 
 			trello_credentials = TrelloCredentials.new(parameters[:public_key], parameters[:access_token])
 			@trello = trello_factory.create(trello_credentials) 
+			@average_cycle_time_calculator = AverageCycleTimeCalculator.new
 		end
 
 		def get(parameters)
 			completed_card_repository = CompletedCardRepository.new(@trello, parameters)
 			finished_cards = completed_card_repository.get
-			return 0 if finished_cards.length == 0
-			return finished_cards[0].cycle_time
+			finished_cards.each do |card|
+				@average_cycle_time_calculator.add(card.cycle_time)
+			end
+			return @average_cycle_time_calculator.average
+		end
+	end
+
+	class AverageCycleTimeCalculator
+		def initialize
+			@cycle_times = []
+		end
+
+		def add(cycle_time)
+			@cycle_times.push(cycle_time)
+		end
+
+		def average
+			return 0 if @cycle_times.length == 0
+			@cycle_times.reduce(:+) / @cycle_times.length
 		end
 	end
 
@@ -181,9 +224,9 @@ module AgileTrello
 
 		def get
 			completed_cards = @card_repository.get_cards_after
-			return [] if completed_cards.length == 0
-			completed_card = @completed_card_factory.create(completed_cards[0])
-			[completed_card]
+			completed_cards.map do |card|
+				@completed_card_factory.create(card)
+			end
 		end
 	end
 end
